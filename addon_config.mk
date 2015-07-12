66a38acbd838521348f01d9ace68b826f48b45d1
Thomas Geissl <thomas.geissl@gmail.com>
2015-07-13 01:44:54 +0200

    added addon config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxStateMachine
+	ADDON_DESCRIPTION = Addon that manages transitions between states
+	ADDON_AUTHOR = Thomas Geissl
+	ADDON_TAGS = "state machine" "fsm" "states" "transitions"
+	ADDON_URL = http://github.com/thomasgeissl/ofxStateMachine
